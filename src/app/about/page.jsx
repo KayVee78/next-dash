e61359f8bb853ceedcdd0b54847a48a0f0518629
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-30 14:45:57 +0530

    About page implementation and styling

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,38 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Studio</h2>
+        <h1 className={styles.title}>
+          Transforming visions into reality with innovative digital solutions
+          and creativity
+        </h1>
+        <p className={styles.desc}>
+          We are dedicated to transforming visions into reality with innovative
+          digital solutions and creativity. Our team of experts leverages
+          cutting-edge technology and imaginative thinking to deliver
+          exceptional results that exceed expectations. Partner with us to
+          experience the power of creativity and innovation in the digital
+          world.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10K +</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>1K +</h1>
+            <p>Clients</p>
+          </div>
+          <div className={styles.box}>
+            <h1>1.5K +</h1>
+            <p>Projects</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="about" fill/>
+        <Image src="/about.png" alt="About Image" fill className={styles.img}/>
       </div>
     </div>
   );
