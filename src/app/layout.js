b66f6232b1f0d1ef412fb1a188db47fb318123e5
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-31 21:10:22 +0530

    Testing and learning theories

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar/NavBar";
 import Footer from "@/components/footer/Footer";
+import ClientSideProvider from "@/components/clientSideProviderTest";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container">
-          <NavBar />
-          {children}
-          <Footer />
-        </div>
+        <ClientSideProvider>
+          <div className="container">
+            <NavBar />
+            {children}
+            <Footer />
+          </div>
+        </ClientSideProvider>
       </body>
     </html>
   );
