fb72362e15ad861059b1af51a71bcdefd8b155a0
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-06-01 11:09:10 +0530

    Mapping sample data fetched from jsonplaceholder test API

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,20 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+const SinglePostPage = async ({ params }) => {
+  console.log(params);
+  const { slug } = params;
+  const post = await getData(slug);
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
@@ -13,9 +26,15 @@ const SinglePostPage = () => {
         />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Mountain Range</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image src="/men-avatar.png" className={styles.avatar} alt="" width={50} height={50} />
+          <Image
+            src="/men-avatar.png"
+            className={styles.avatar}
+            alt=""
+            width={50}
+            height={50}
+          />
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Author</span>
             <span className={styles.detailValue}>Samson Fernando</span>
@@ -25,11 +44,7 @@ const SinglePostPage = () => {
             <span className={styles.detailValue}>01.01.2024</span>
           </div>
         </div>
-        <div className={styles.content}>
-          The majestic snow-capped mountain peaks glisten under the sun,
-          surrounded by serene, pristine landscapes, creating a breathtaking,
-          tranquil view.
-        </div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
